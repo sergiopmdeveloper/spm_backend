cb1bb123d2cb66d45d163c32054d85079823698f
Sergio <sergio.pm.developer@gmail.com>
2024-07-14 15:59:18 +0000

    feat: get projects from the projects API endpoint

--- spm_backend/api.py
+++ spm_backend/api.py
@@ -1,8 +1,8 @@
 from django.core.handlers.wsgi import WSGIRequest
 from ninja import NinjaAPI
 
-from content.models import Job, Study
-from content.schemas import JobOut, StudyOut
+from content.models import Job, Project, Study
+from content.schemas import JobOut, ProjectOut, StudyOut
 
 api = NinjaAPI()
 
@@ -33,3 +33,17 @@ def get_jobs(request: WSGIRequest):
     """
 
     return Job.objects.all()
+
+
+@api.get("/projects", response=list[ProjectOut])
+def get_projects(request: WSGIRequest):
+    """
+    Get projects
+
+    Parameters
+    ----------
+    request : WSGIRequest
+        The request object, not used
+    """
+
+    return Project.objects.all()
